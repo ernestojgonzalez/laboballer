14920811b962b31b178b47e66c2214fa06c6e911
ernesto jose gonzalez carrero <ernesto.jose.gonzalez.c@gmail.com>
2012-07-06 00:07:26 -0430

    se agrego plantillas a todos los modulos
    falta mejorar las tablas de informacion de casi todos los modulos

---
+++ menu.php
@@ -0,0 +1,62 @@
+    <!--* MENU DEL SISTEMA DANIEL *-->
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="ne-us" xml:lang="en">
+<div class="art-nav-outer" align="center">
+	<div class="l"></div>
+    <div class="r"></div>
+	
+	<ul class="art-hmenu">
+		<li>
+			<a href="index.php" class="active"><span class="l"></span><span class="r"></span><span class="t">Inicio</span></a>
+		</li>	
+		<li>
+			<a href="#"><span class="l"></span><span class="r"></span><span class="t">Empleados</span></a>
+		<ul>
+				 <li><a href="form_empleado.php">Registro</a></a></li>
+                 <li><a href="consulta_empleado.php">Consulta General</a></li>
+				 <li><a href="consultacriterio_empleado.php">Consulta Criterios</a></li>
+				 <li><a href="modificar_empleado1.php">Modificar Empleado</a></a></li>
+                 <li><a href="eliminarempleado.php">Eliminar Empleado </a></li> 
+        </ul>
+		</li>	
+		<li>
+			<a href="#"><span class="l"></span><span class="r"></span><span class="t">Equipos</span></a>
+		<ul>
+				 <li><a href="form_equipo.php">Registro</a></li>
+                 <li><a href="consulta_equipo.php">Consulta General</a></li>
+				 <li><a href="consultacriterio_equipo.php">Consulta Criterio</a></li>
+				 <li><a href="modificar_equipo1.php">Modificar Equipo </a></li>
+                 <li><a href="eliminarequipo.php">Eliminar Equipo </a></li> 
+        </ul>
+		</li>
+		<li>
+			<a href="#"><span class="l"></span><span class="r"></span><span class="t">Piezas</span></a>
+		<ul>
+				 <li><a href="form_parte_pieza.php">Registro</a></li>
+				 <li><a href="consultacriterio_equipo.php">Consulta de partes y piezas</a></li>
+				 <li><a href="#">Modificar partes</a></li>
+                 <li><a href="eliminar_partes.php">Eliminar partes de los equipos</a></li> 
+        </ul>
+		</li>	
+		<li>
+			<a href="#"><span class="l"></span><span class="r"></span><span class="t">Orden de Trabajo</span></a>
+			<ul>
+				 <li><a href="form_odt.php">Registrar Orden de Trabajo</a></li>
+				 <li><a href="#">Consultar</a></li>
+				 <li><a href="#">Modificar</a></li>
+				 <li><a href="#">Eliminar</a></li>
+        </ul>
+		</li>
+		<li>
+			<a href="#"><span class="l"></span><span class="r"></span><span class="t">Salir</span></a>
+		</li>			
+	</ul>
+</div>
+</div>
+<div class="cleared reset-box"></div>
+<div class="art-content-layout">
+                <div class="art-content-layout-row">
+                    <div class="art-layout-cell art-content">
+<div class="art-post">
+    <div class="art-post-body">
+<div class="art-post-inner art-article">
--- modificar_empleado2.php
+++ modificar_empleado2.php
@@ -12,9 +12,102 @@
 <script type="text/javascript" src="jquery.js"></script>
 <script language="javascript" src="validacion.js"></script> 
 
+  <!-- plantilla  --> 
+	<meta name="description" content="Description" />
+    <meta name="keywords" content="Keywords" />    
+	<link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+<!-- fin plantilla  -->
+   
+
+    <!--[if IE 6]><link rel="stylesheet" href="style.ie6.css" type="text/css" media="screen" /><![endif]-->
+    <!--[if IE 7]><link rel="stylesheet" href="style.ie7.css" type="text/css" media="screen" /><![endif]-->
+
+    <script type="text/javascript" src="jquery.js"></script>
+    <script type="text/javascript" src="script.js"></script>
 </head>
 <body>
-
+<!--INICIO DE LA PLANTILLA -->
+<!-- plantilla  --> 
+	<meta name="description" content="Description" />
+    <meta name="keywords" content="Keywords" />    
+	<link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+<!-- fin plantilla  -->
+<div id="art-page-background-glare">
+    <div id="art-page-background-glare-image"> </div>
+</div>
+<div id="art-main">
+    <div class="art-header">
+        <div class="art-header-clip">
+        <div class="art-header-center">
+            <div class="art-header-png"></div>
+            <div class="art-header-jpeg"></div>
+        </div>
+        </div>
+    <div class="art-header-wrapper">
+    <div class="art-header-inner">
+        <div class="art-textblock"> </div>
+        <div class="art-headerobject"></div>
+        <script type="text/javascript" src="swfobject.js"></script>
+        <div id="art-flash-area">
+        <div id="art-flash-container">
+        <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="788" height="150" id="art-flash-object">
+        	<param name="movie" value="container.swf" />
+        	<param name="quality" value="high" />
+        	<param name="scale" value="default" />
+        	<param name="wmode" value="transparent" />
+        	<param name="flashvars" value="color1=0xFFFFFF&amp;alpha1=.50&amp;framerate1=24&amp;loop=true&amp;wmode=transparent&amp;clip=images/flash.swf&amp;radius=20&amp;clipx=0&amp;clipy=-36&amp;initalclipw=900&amp;initalcliph=255&amp;clipw=788&amp;cliph=223&amp;width=788&amp;height=150&amp;textblock_width=0&amp;textblock_align=no&amp;hasTopCorners=true&amp;hasBottomCorners=true" />
+            <param name="swfliveconnect" value="true" />
+        	<!--[if !IE]>-->
+        	<object type="application/x-shockwave-flash" data="container.swf" width="788" height="150">
+        	    <param name="quality" value="high" />
+        	    <param name="scale" value="default" />
+        	    <param name="wmode" value="transparent" />
+            	<param name="flashvars" value="color1=0xFFFFFF&amp;alpha1=.50&amp;framerate1=24&amp;loop=true&amp;wmode=transparent&amp;clip=images/flash.swf&amp;radius=20&amp;clipx=0&amp;clipy=-36&amp;initalclipw=900&amp;initalcliph=255&amp;clipw=788&amp;cliph=223&amp;width=788&amp;height=150&amp;textblock_width=0&amp;textblock_align=no&amp;hasTopCorners=true&amp;hasBottomCorners=true" />
+                <param name="swfliveconnect" value="true" />
+        	<!--<![endif]-->
+        		<div class="art-flash-alt"><a href="http://www.adobe.com/go/getflashplayer"><img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="Get Adobe Flash player" /></a></div>
+        	<!--[if !IE]>-->
+        	</object>
+        	<!--<![endif]-->
+        </object>
+        </div>
+        </div>
+        <script type="text/javascript">    swfobject.switchOffAutoHideShow(); swfobject.registerObject("art-flash-object", "9.0.0", "expressInstall.swf");</script>
+        <div class="art-logo">
+                 <h1 class="art-logo-name"><a href="./index.html">LABOBALLER  </a></h1>
+                         <h2 class="art-logo-text"> BARRY WEHMILLER  Lavadora de botellas</h2>
+                </div>
+    </div>
+    </div>
+    </div>
+    <div class="cleared reset-box"></div>
+    <div class="art-sheet">
+        <div class="art-sheet-tl"></div>
+        <div class="art-sheet-tr"></div>
+        <div class="art-sheet-bl"></div>
+        <div class="art-sheet-br"></div>
+        <div class="art-sheet-tc"></div>
+        <div class="art-sheet-bc"></div>
+        <div class="art-sheet-cl"></div>
+        <div class="art-sheet-cr"></div>
+        <div class="art-sheet-cc"></div>
+        <div class="art-sheet-body">
+<div class="art-nav">
+	<div class="art-nav-l"></div>
+	<div class="art-nav-r"></div>
+<?php include "menu.php" ?>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<h2 class="art-postheader" align="center">LABOBALLER</h2>
+<p>&nbsp;</p>                
+                                
+								
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+                <div class="cleared"></div>
+        <div class="art-postcontent">
+<!-- EMPIEZA LA PAG -->
 
     <div align="center">
       <?php
@@ -36,7 +129,7 @@
                    if($nro_fil == 0)
                    {
 
-                   echo "<center><table><tr><td><br><br><strong><center>El empleado $criterio no existe en nuestra base de datos <br> por favor valide sus datos.</center></strong></td></tr></table></center>";
+                   echo "<center><br><br><strong><center>El empleado $criterio no existe en nuestra base de datos <br> por favor valide sus datos.</center></strong></center><br></br><br></br>";
                    }else
                    {
                    while ($row = mysql_fetch_row($resultado)){
@@ -200,6 +293,44 @@ Presione el botón continuar para guardar los cambios</strong></div></td></tr>
                    ?>
 
 
+   
+<!-- FINALIZA LA PAG -->
+<!-- PIE DE PAG -->
+              </div> </div>  
+			   <div class="cleared"></div>
+                </div>
+
+		<div class="cleared"></div>
     </div>
+</div>
+
+                      <div class="cleared"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="cleared"></div>
+            <div class="art-footer">
+                <div class="art-footer-t"></div>
+                <div class="art-footer-l"></div>
+                <div class="art-footer-b"></div>
+                <div class="art-footer-r"></div>
+                <div class="art-footer-body">
+                            <div class="art-footer-text">
+                                
+<p><a href="#">Link1</a> | <a href="#">Link2</a> | <a href="#">Link3</a></p>
+<p>Derechos reservados en Espanol</p>
+
+
+                                                            </div>
+                    <div class="cleared"></div>
+                </div>
+            </div>
+    		<div class="cleared"></div>
+        </div>
+    </div>
+    <div class="cleared"></div>
+    <p class="art-page-footer"><a href="http://www.artisteer.com/?p=website_templates">Website Template</a> created with Artisteer.</p>
+</div>
+
 </body>
 </html>
